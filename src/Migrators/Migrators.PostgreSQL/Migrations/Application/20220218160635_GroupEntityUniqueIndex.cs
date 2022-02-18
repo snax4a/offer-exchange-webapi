f8874ce78d3013f5b5c0d6786e9b1fac1ac1e0cc
Szymon Sus <sus.szymon3@gmail.com>
2022-02-18 17:07:44 +0100

    Added unique index to Group entity

---
+++ src/Migrators/Migrators.PostgreSQL/Migrations/Application/20220218160635_GroupEntityUniqueIndex.cs
@@ -0,0 +1,27 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace Migrators.PostgreSQL.Migrations.Application
+{
+    public partial class GroupEntityUniqueIndex : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.CreateIndex(
+                name: "IX_Groups_Name_CreatedBy",
+                schema: "Catalog",
+                table: "Groups",
+                columns: new[] { "Name", "CreatedBy" },
+                unique: true);
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropIndex(
+                name: "IX_Groups_Name_CreatedBy",
+                schema: "Catalog",
+                table: "Groups");
+        }
+    }
+}
