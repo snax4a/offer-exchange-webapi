f089316a7a9e26fb025dd674fec91b0fc0c2a893
Szymon Sus <sus.szymon3@gmail.com>
2022-03-24 18:54:11 +0100

    fix: add DateOnly json converter

--- src/Core/Domain/Exchange/InquiryProduct.cs
+++ src/Core/Domain/Exchange/InquiryProduct.cs
@@ -4,12 +4,12 @@ public class InquiryProduct : AuditableEntity, IAggregateRoot
 {
     public string Name { get; private set; } = default!;
     public int Quantity { get; private set; }
-    public DateTime PreferredDeliveryDate { get; private set; }
+    public DateOnly PreferredDeliveryDate { get; private set; }
     public Guid InquiryId { get; private set; }
     public virtual Inquiry Inquiry { get; private set; } = default!;
     public ICollection<OfferProduct> OfferProducts { get; private set; } = new List<OfferProduct>();
 
-    public InquiryProduct(Guid inquiryId, string name, int quantity, DateTime preferredDeliveryDate)
+    public InquiryProduct(Guid inquiryId, string name, int quantity, DateOnly preferredDeliveryDate)
     {
         InquiryId = inquiryId;
         Name = name;
