c256fc914b551838fd89ef3597888331a9561b6e
Szymon Sus <sus.szymon3@gmail.com>
2022-03-25 19:33:24 +0100

    fix: offer product gross value calculation

--- src/Core/Domain/Exchange/OfferProduct.cs
+++ src/Core/Domain/Exchange/OfferProduct.cs
@@ -7,7 +7,7 @@ public class OfferProduct : BaseEntity, IAggregateRoot
     public int Quantity { get; private set; }
     public decimal NetPrice { get; private set; }
     public decimal NetValue => Quantity * NetPrice;
-    public decimal GrossValue => Quantity * NetPrice * (1 + VatRate ?? 0);
+    public decimal GrossValue => Quantity * NetPrice * (1 + (VatRate ?? 0));
     public DateOnly DeliveryDate { get; private set; }
     public bool IsReplacement { get; private set; }
     public string? ReplacementName { get; private set; }
