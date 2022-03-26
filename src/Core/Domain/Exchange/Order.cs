8bed831a96365951d66f921f637f0e53df200a94
Szymon Sus <sus.szymon3@gmail.com>
2022-03-26 14:56:54 +0100

    refactor(Order): use ulong instead of decimals

--- src/Core/Domain/Exchange/Order.cs
+++ src/Core/Domain/Exchange/Order.cs
@@ -4,8 +4,8 @@ namespace FSH.WebApi.Domain.Exchange;
 
 public class Order : AuditableEntity, IAggregateRoot
 {
-    public decimal NetValue { get; private set; }
-    public decimal GrossValue { get; private set; }
+    public ulong NetValue { get; private set; }
+    public ulong GrossValue { get; private set; }
     public string CurrencyCode { get; private set; } = default!;
     public DeliveryCost DeliveryCost { get; private set; } = default!;
     public Guid TraderId { get; private set; }
@@ -23,8 +23,8 @@ public class Order : AuditableEntity, IAggregateRoot
         if (offerProducts.Count == 0) throw new ArgumentException("Cannot be empty list", nameof(offerProducts));
 
         Id = NewId.Next().ToGuid();
-        NetValue = offerProducts.Sum(op => op.NetValue);
-        GrossValue = offerProducts.Sum(op => op.GrossValue);
+        NetValue = offerProducts.Aggregate(0UL, (a, c) => a + c.NetValue);
+        GrossValue = offerProducts.Aggregate(0UL, (a, c) => a + c.GrossValue);
         CurrencyCode = offerProducts[0].CurrencyCode;
         Status = OrderStatus.Waiting;
         TraderId = traderId;
