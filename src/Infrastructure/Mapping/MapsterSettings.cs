bde1b017e438f2ef450052dc9947906487a2011f
Szymon Sus <sus.szymon3@gmail.com>
2022-04-08 12:57:44 +0200

    chore: clean mapster settings file

--- src/Infrastructure/Mapping/MapsterSettings.cs
+++ src/Infrastructure/Mapping/MapsterSettings.cs
@@ -12,11 +12,8 @@ public class MapsterSettings
 {
     public static void Configure()
     {
-        // here we will define the type conversion / Custom-mapping
+        // Here we will define the type conversion / Custom-mapping
         // More details at https://github.com/MapsterMapper/Mapster/wiki/Custom-mapping
-
-        // This one is actually not necessary as it's mapped by convention
-        // TypeAdapterConfig<Product, ProductDto>.NewConfig().Map(dest => dest.BrandName, src => src.Brand.Name);
 
         TypeAdapterConfig<Group, GroupDetailsDto>.NewConfig()
             .Map(dest => dest.Traders, src => src.TraderGroups.Select(tg => tg.Trader));
