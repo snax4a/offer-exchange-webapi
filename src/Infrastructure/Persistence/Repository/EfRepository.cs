3cb9277b7c2a4a77b41421441400bce98846690d
Szymon Sus <sus.szymon3@gmail.com>
2022-04-20 10:57:24 +0200

    EfRepository.ApplySpecification => only use mapster when no selector is defined

--- src/Infrastructure/Persistence/Repository/EfRepository.cs
+++ src/Infrastructure/Persistence/Repository/EfRepository.cs
@@ -19,6 +19,8 @@ public class EfRepository<T> : RepositoryBase<T>, IReadRepository<T>, IRepositor
     // We override the default behavior when mapping to a dto.
     // We're using Mapster's ProjectToType here to immediately map the result from the database.
     protected override IQueryable<TResult> ApplySpecification<TResult>(ISpecification<T, TResult> specification) =>
-        ApplySpecification(specification, false)
-            .ProjectToType<TResult>();
+        specification.Selector is not null
+            ? base.ApplySpecification(specification)
+            : ApplySpecification(specification, false)
+                .ProjectToType<TResult>();
 }
