d96e9c2b50e43a8846d96366a91bebbbf1cf86c7
Szymon Sus <sus.szymon3@gmail.com>
2022-07-30 19:38:44 +0200

    feat: add admin controller

--- src/Core/Application/Common/Persistence/IDapperRepository.cs
+++ src/Core/Application/Common/Persistence/IDapperRepository.cs
@@ -11,9 +11,19 @@ public interface IDapperRepository : ITransientService
     /// <param name="sql">The sql string.</param>
     /// <param name="param">The paramters in the sql string.</param>
     /// <param name="transaction">The transaction to be performed.</param>
-    /// <param name="cancellationToken">The <see cref="CancellationToken"/> to observe while waiting for the task to complete.</param>
     /// <returns>Returns <see cref="Task"/> of <see cref="IReadOnlyCollection{T}"/>.</returns>
-    Task<IReadOnlyList<T>> QueryAsync<T>(string sql, object? param = null, IDbTransaction? transaction = null, CancellationToken cancellationToken = default)
+    Task<IEnumerable<T>> QueryAsync<T>(string sql, object? param = null, IDbTransaction? transaction = null)
+    where T : class;
+
+    /// <summary>
+    /// Get a <typeparamref name="T"/> using raw sql string with parameters.
+    /// </summary>
+    /// <typeparam name="T">The type of the entity.</typeparam>
+    /// <param name="sql">The sql string.</param>
+    /// <param name="param">The paramters in the sql string.</param>
+    /// <param name="transaction">The transaction to be performed.</param>
+    /// <returns>Returns <see cref="Task"/> of <typeparamref name="T"/>.</returns>
+    Task<T?> QueryFirstOrDefaultAsync<T>(string sql, object? param = null, IDbTransaction? transaction = null)
     where T : class, IEntity;
 
     /// <summary>
@@ -23,20 +33,7 @@ public interface IDapperRepository : ITransientService
     /// <param name="sql">The sql string.</param>
     /// <param name="param">The paramters in the sql string.</param>
     /// <param name="transaction">The transaction to be performed.</param>
-    /// <param name="cancellationToken">The <see cref="CancellationToken"/> to observe while waiting for the task to complete.</param>
     /// <returns>Returns <see cref="Task"/> of <typeparamref name="T"/>.</returns>
-    Task<T?> QueryFirstOrDefaultAsync<T>(string sql, object? param = null, IDbTransaction? transaction = null, CancellationToken cancellationToken = default)
-    where T : class, IEntity;
-
-    /// <summary>
-    /// Get a <typeparamref name="T"/> using raw sql string with parameters.
-    /// </summary>
-    /// <typeparam name="T">The type of the entity.</typeparam>
-    /// <param name="sql">The sql string.</param>
-    /// <param name="param">The paramters in the sql string.</param>
-    /// <param name="transaction">The transaction to be performed.</param>
-    /// <param name="cancellationToken">The <see cref="CancellationToken"/> to observe while waiting for the task to complete.</param>
-    /// <returns>Returns <see cref="Task"/> of <typeparamref name="T"/>.</returns>
-    Task<T> QuerySingleAsync<T>(string sql, object? param = null, IDbTransaction? transaction = null, CancellationToken cancellationToken = default)
+    Task<T> QuerySingleAsync<T>(string sql, object? param = null, IDbTransaction? transaction = null)
     where T : class, IEntity;
 }
---
+++ src/Host/Controllers/Admin/AdminController.cs
@@ -0,0 +1,15 @@
+﻿using FSH.WebApi.Application.Admin;
+using FSH.WebApi.Application.Admin.DTOs;
+
+namespace FSH.WebApi.Host.Controllers.Admin;
+
+public class AdminController : VersionedApiController
+{
+    [HttpPost("create-missing-customers")]
+    [MustHavePermission(ResourceAction.Manage, Resource.Customers)]
+    [OpenApiOperation("Create missing customers for users who don't have.", "")]
+    public Task<CreateCustomersResponse> CreateCustomersAsync()
+    {
+        return Mediator.Send(new CreateCustomersRequest());
+    }
+}
